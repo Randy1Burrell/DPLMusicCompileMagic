bd7e47f37c98b760377d36e2f2159b8708d6b005
SirrJohns <sirericjohns@hotmail.com>
2016-03-22 19:48:28 -0500

    Added files via upload

--- Headerfiles/SyntaxAnalysis.h
+++ Headerfiles/SyntaxAnalysis.h
@@ -1,40 +1,49 @@
 #if !defined(_SA_)
 #define _SA_
 
-
 #include <iostream>
+#include <fstream>
 #include <string>
 #include "LexicalAnalysis.h"
+#include "tree.h"
+#include "node.h"
 
 using namespace std;
+
 /*
-	STTAG ::= LEFT_Bracket IDENTIFIER RIGHT_Bracket
-	ENTAG ::= LEFT_Bracket BACK_SLASH IDENTIFIER RIGHT_Bracket
-	EXPR ::= (STTAG IDENTIFIER {+ IDENTIFIER} ENTAG )| (STTAG SENT {+ SENT} ENTAG)
-	
-	SENT ::= IDENTIFIER {+ SENT} (PERIOD | QUESTION_MARK | EXCLAMATION)
+
+	STTAG ::= LEFT_Bracket SENT RIGHT_Bracket
+
+	ENTAG ::= LEFT_Bracket BACK_SLASH SENT RIGHT_Bracket
+
+	EXPR ::= STTAG SENT ENTAG
+
+	SENT ::= IDENTIFIER {+ SENT}
+
   */
 
 class SyntaxAnalysis  
 {
 public:
-	void syntax();
-	void setNewInput(string in);
-	SyntaxAnalysis(string in);
-	virtual ~SyntaxAnalysis();
-
-private:
-	void exp();
-	void strtag(); //START TAG
-	void endtag(); //END TAG
-	void sent();	//SENTENCE
+	SyntaxAnalysis();
+    virtual ~SyntaxAnalysis();
 	
-
+    string Dictionary(string x);
+	node * buildTree(string  fileName, ostream &results);
+	bool parseTree(node *, ostream &SyntaxResults, int counter);
+	
+    void syntax();
+    void setNewInput(string in);
+    SyntaxAnalysis(string in);
 
 private:
 	LexicalAnalysis LA;
 	int nextToken;
+    
+    void exp();
+    void strtag(); //START TAG
+    void endtag(); //END TAG
+    void sent();	//SENTENCE
 };
-
 
 #endif
